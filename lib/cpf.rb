a4e0795220e2c005c0ac38989dfd823665372db5
vinibispo <vini.bispo015@gmail.com>
2021-10-09 11:16:29 -0300

    fix: fix wrong algorithm

--- lib/cpf.rb
+++ lib/cpf.rb
@@ -19,21 +19,26 @@ class Cpf
 
     return false unless cpf.length == 11
 
-    cpf_array_numerico_com_indice = cpf
-                                    .chars
-                                    .map(&:to_i)
-                                    .each_with_index
-    primeira_soma = cpf_array_numerico_com_indice
+    cpf_array = cpf.chars
+    primeira_soma = cpf_array[..8]
+                    .map(&:to_i)
+                    .each_with_index
                     .map { |numero, indice| numero * (indice + 1) }
                     .inject(:+)
-    segunda_soma = cpf_array_numerico_com_indice
-                  .map { |numero, indice| numero * indice }
-                  .inject(:+)
+
+    segunda_soma = cpf_array[..9]
+                    .map(&:to_i)
+                    .each_with_index
+                    .map { |numero, indice| numero * indice }
+                    .inject(:+)
 
     primeiro_verificador, segundo_verificador = [primeira_soma, segunda_soma]
                                                 .map(&VERIFICADOR_SOMA)
     
-    return true if(primeiro_verificador === cpf[9] && segundo_verificador == cpf[10])
+    if primeiro_verificador === cpf[9].to_i && segundo_verificador == cpf[10].to_i
+      return true
+    end
+
     false
   end
 end
