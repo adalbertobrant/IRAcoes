799bac120252be51c54550ef288223e88d17c8d2
vinibispo <vini.bispo015@gmail.com>
2021-10-09 10:21:50 -0300

    feat: almost finishing verifica_cpf method

--- lib/cpf.rb
+++ lib/cpf.rb
@@ -3,5 +3,28 @@ def verifica_cpf(cpf)
 
   return false unless cpf.length == 11
 
-  true
+  cpf_array_numerico_com_indice = cpf
+                                  .chars
+                                  .map(&:to_i)
+                                  .each_with_index
+  primeira_soma = cpf_array_numerico_com_indice
+                  .map { |numero, indice| numero * (indice + 1) }
+                  .inject(:+)
+  segunda_soma = cpf_array_numerico_com_indice
+                 .map { |numero, indice| numero * indice }
+                 .inject(:+)
+
+  primeiro_verificador, segundo_verificador = [primeira_soma, segunda_soma]
+                                              .map(&verificador_soma)
+  
+  return true if(primeiro_verificador === cpf[9] && segundo_verificador == cpf[10])
+  false
+end
+
+
+verificador_soma = -> (soma) do
+  raise TypeError unless a.is_a?(Number)
+
+  resto_divisao = soma % 11
+  resto_divisao == 10 ? 0 : resto_divisao
 end
