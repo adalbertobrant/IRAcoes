bed85045713930c25a1e268be3c239b0f7c9790b
vinibispo <vini.bispo015@gmail.com>
2021-10-09 10:32:36 -0300

    test: add helper method to don't need change all

--- test/cpf_test.rb
+++ test/cpf_test.rb
@@ -1,5 +1,10 @@
 require 'minitest/autorun'
 require_relative '../lib/cpf'
+
+def verifica_cpf(cpf)
+  Cpf.new(cpf).verifica_cpf
+end
+
 class CpfTest < Minitest::Test
   def test_if_cpf_is_not_a_number
     number_cpf = rand(11111111111..99999999999)
