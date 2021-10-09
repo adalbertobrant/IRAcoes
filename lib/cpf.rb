8c344da6c5b43c6ca0d46d92b3a7c55085f81dab
vinibispo <vini.bispo015@gmail.com>
2021-10-09 10:24:36 -0300

    feat: add class instead of simple function

--- lib/cpf.rb
+++ lib/cpf.rb
@@ -1,30 +1,38 @@
-def verifica_cpf(cpf)
-  return false unless cpf.is_a?(String)
+class Cpf
+  attr_reader :cpf
 
-  return false unless cpf.length == 11
+  def initialize(cpf)
+    @cpf = cpf
+  end
 
-  cpf_array_numerico_com_indice = cpf
-                                  .chars
-                                  .map(&:to_i)
-                                  .each_with_index
-  primeira_soma = cpf_array_numerico_com_indice
-                  .map { |numero, indice| numero * (indice + 1) }
+  def verifica_cpf
+    return false unless cpf.is_a?(String)
+
+    return false unless cpf.length == 11
+
+    cpf_array_numerico_com_indice = cpf
+                                    .chars
+                                    .map(&:to_i)
+                                    .each_with_index
+    primeira_soma = cpf_array_numerico_com_indice
+                    .map { |numero, indice| numero * (indice + 1) }
+                    .inject(:+)
+    segunda_soma = cpf_array_numerico_com_indice
+                  .map { |numero, indice| numero * indice }
                   .inject(:+)
-  segunda_soma = cpf_array_numerico_com_indice
-                 .map { |numero, indice| numero * indice }
-                 .inject(:+)
 
-  primeiro_verificador, segundo_verificador = [primeira_soma, segunda_soma]
-                                              .map(&verificador_soma)
-  
-  return true if(primeiro_verificador === cpf[9] && segundo_verificador == cpf[10])
-  false
-end
+    primeiro_verificador, segundo_verificador = [primeira_soma, segunda_soma]
+                                                .map(&verificador_soma)
+    
+    return true if(primeiro_verificador === cpf[9] && segundo_verificador == cpf[10])
+    false
+  end
 
 
-verificador_soma = -> (soma) do
-  raise TypeError unless a.is_a?(Number)
+  verificador_soma = -> (soma) do
+    raise TypeError unless soma.is_a?(Number)
 
-  resto_divisao = soma % 11
-  resto_divisao == 10 ? 0 : resto_divisao
+    resto_divisao = soma % 11
+    resto_divisao == 10 ? 0 : resto_divisao
+  end
 end
